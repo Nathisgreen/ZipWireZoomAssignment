bfb8e21bd28114aaedb84d99a257ee7b8ced372f
Nathan <treesbegreen@hotmail.com>
2015-04-04 06:01:08 +0100

    added basic player controls

--- Assets/scPlayerController.cs
+++ Assets/scPlayerController.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class scPlayerController : MonoBehaviour {
 
+    private float moveForce = 30;
+    private float maxHozVelocity = 5f;
+
 	// Use this for initialization
 	void Start () {
-        this.rigidbody.AddForce(new Vector3(500, 0, 0));
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x <= 1){
+            rigidbody.AddForce(-rigidbody.transform.right * moveForce, ForceMode.Force);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x >=-1) {
+            rigidbody.AddForce(rigidbody.transform.right * moveForce, ForceMode.Force);
+        }
+
+        if (rigidbody.velocity.x > maxHozVelocity) {
+            rigidbody.velocity = new Vector3(maxHozVelocity, rigidbody.velocity.y, rigidbody.velocity.z);
+        }
+
+        if (rigidbody.velocity.x < -maxHozVelocity) {
+            rigidbody.velocity = new Vector3(-maxHozVelocity, rigidbody.velocity.y, rigidbody.velocity.z);
+        }
+
 	}
 }
