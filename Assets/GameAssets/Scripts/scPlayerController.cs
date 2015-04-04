fb95c3da878c5b7b0105f881479213ae8fe2945c
Nathan <treesbegreen@hotmail.com>
2015-04-04 12:53:10 +0100

    Added line renderer for player zip line and restructured project layout

---
+++ Assets/GameAssets/Scripts/scPlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scPlayerController : MonoBehaviour {
+
+    private const float HorizontalInputDeadZone =1f;
+    private const float HorizontalMoveForce = 50f;
+    private const float MaxHorizontalVelocity = 5f;
+    
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x <= HorizontalInputDeadZone) {
+            rigidbody.AddForce(-rigidbody.transform.right * HorizontalMoveForce, ForceMode.Force);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x >= -HorizontalInputDeadZone) {
+            rigidbody.AddForce(rigidbody.transform.right * HorizontalMoveForce, ForceMode.Force);
+
+        }
+
+        if (rigidbody.velocity.x > MaxHorizontalVelocity) {
+            rigidbody.velocity = new Vector3(MaxHorizontalVelocity, rigidbody.velocity.y, rigidbody.velocity.z);
+        }
+
+        if (rigidbody.velocity.x < -MaxHorizontalVelocity) {
+            rigidbody.velocity = new Vector3(-MaxHorizontalVelocity, rigidbody.velocity.y, rigidbody.velocity.z);
+        }
+
+	}
+}
---
+++ Assets/GameAssets/Scripts/scPlayerRope.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scPlayerRope : MonoBehaviour {
+
+    public GameObject startPosition;
+    public GameObject endPosition;
+    public Material lineMaterial;
+
+    private LineRenderer lineRenderer;
+
+
+	// Use this for initialization
+	void Start () {
+
+        lineRenderer = this.gameObject.AddComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, startPosition.transform.position);
+        lineRenderer.SetPosition(1, endPosition.transform.position);
+
+        lineRenderer.SetWidth(0.1f, 0.05f);
+        lineRenderer.material = lineMaterial;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lineRenderer.SetPosition(0, startPosition.transform.position);
+
+	}
+}
