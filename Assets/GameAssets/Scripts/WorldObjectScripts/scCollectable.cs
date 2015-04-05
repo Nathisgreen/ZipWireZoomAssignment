4d67fe5948412b57a0498ad25ea99df7d83436dc
Nathan <treesbegreen@hotmail.com>
2015-04-05 21:42:10 +0100

    Added scorebox background and particles when collecting suns

--- Assets/GameAssets/Scripts/WorldObjectScripts/scCollectable.cs
+++ Assets/GameAssets/Scripts/WorldObjectScripts/scCollectable.cs
@@ -5,22 +5,29 @@ public class scCollectable : MonoBehaviour {
 
     private scLevelController levelController;
     private bool isHit = false;
+    public GameObject particleSystemToPlay;
 
 	void Start () {
         levelController = GameObject.FindGameObjectWithTag("LevelController").GetComponent<scLevelController>();
-	}
-	
-	void Update () {
-	
 	}
 
     void OnTriggerEnter( Collider other){
         if (other.collider.tag == "Player"){
             if (!isHit){
                 levelController.addToScore(10);
+
+                if (particleSystemToPlay != null) {
+                    createParticleSystem();
+                }
                 Destroy(this.gameObject);
                 isHit = true;
             }
         }
+    }
+
+    private void createParticleSystem(){
+        GameObject part = (GameObject)GameObject.Instantiate(particleSystemToPlay, transform.position, Quaternion.identity);
+        part.transform.position = transform.position;
+        part.AddComponent<scParticleController>();
     }
 }
---
+++ Assets/GameAssets/Scripts/scParticleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scParticleController : MonoBehaviour {
+
+    private ParticleSystem ps;
+
+	void Start () {
+        ps = this.GetComponent<ParticleSystem>();
+	}
+	
+	void Update () {
+        if (ps != null) {
+            if (!ps.IsAlive()) {
+                Destroy(this.gameObject);
+            }
+        }
+	}
+}
